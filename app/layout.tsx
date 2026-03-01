152a4a3f423cd6ef5662b1088f40f97d911c7962
Peter Junior <pierreouma@gmail.com>
2026-03-01 18:56:28 +0300

    fix: update Next.js to version 16.1.6 for improved performance and compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,23 +14,23 @@ const geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Netily - Fast & Easy Internet Payments",
   description: "Pay your internet bills faster and easier with Netily. Access customer support instantly.",
-  generator: "v0.app",
+  generator: "ojpierre",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/",
   },
 }
 
